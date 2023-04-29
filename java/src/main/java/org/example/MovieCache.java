6c65d82c339c6f56bd65731d86f6c9bb5e95c2ec
kosystephy <kosysteve03@gmail.com>
2023-04-29 22:19:19 +0100

    cache done

--- java/src/main/java/org/example/MovieCache.java
+++ java/src/main/java/org/example/MovieCache.java
@@ -1,22 +1,22 @@
 package org.example;
-//import java.util.HashSet;
-//
-//public class MovieCache {
-//	private HashSet<Integer> movieId;
-//
-//	public MovieCache(){
-//		this.movieId=new HashSet<>();
-//	}
-//	public boolean contains (int movieId)
-//	{
-//				return movieId.contains(movieId);
-//			}
-//			public void add(int movieId)
-//			{
-//				movieId.add(movieId);
-//			}
-//			public void remove(int movieId)
-//			{
-//				movieId.remove(movieId);
-//			}
-//}
+import java.util.HashSet;
+
+public class MovieCache {
+	private HashSet<Integer> movieIds;
+
+	public MovieCache(){
+		this.movieIds=new HashSet<>();
+	}
+	public boolean contains (int movieId)
+	{
+				return movieIds.contains(movieId);
+			}
+			public void add(int movieId)
+			{
+				movieIds.add(movieId);
+			}
+			public void remove(int movieId)
+			{
+				movieIds.remove(movieId);
+			}
+}
